c276f2cd464927c7258b4f18a66ed2320c19303e
Utkarsha015 <114806606+Utkarsha015@users.noreply.github.com>
2025-08-16 22:13:44 +0530

    Create Reports.jsx

---
+++ frontend/src/pages/Treasurer/Reports.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Reports = () => {
+  return (
+    <div>
+      <h2>Reports</h2>
+      <p>Monthly and quarterly finance reports.</p>
+      <div className="chart-box">📊 Bar Chart Placeholder</div>
+      <button className="btn">Download Report (PDF)</button>
+    </div>
+  );
+};
+
+export default Reports;
